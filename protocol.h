7dde74aaade54d49856aefbcaf7b2f0b0bf7802c
joshuagame <joshuagame@gmail.com>
2015-09-17 15:28:40 +0200

    new project structure

--- protocol.h
+++ protocol.h
@@ -1,6 +1,34 @@
-//
-// Created by joshuagame on 17/09/15.
-//
+/*
+**     _    ____  _  __
+**    / \  / ___|| |/ /  ASK
+**   / _ \ \___ \| ' /   Authentication Sessions Keeper Server
+**  / ___ \ ___) | . \   -------------------------------------
+** /_/   \_\____/|_|\_\  www.codegazoline.it/ask
+**                 v0.1
+** ===========================================================================
+**
+** Copyright (C) 2015, The CodeGazoline Team - gargantua@codegazoline.it
+**
+** protocol.h
+** header for HTTP and routes management functionalities
+**
+** This file is part of the ASK Server.
+**
+** The ASK Server is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** The ASK Server is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Nome - Programma.If not, see <http:**www.gnu.org/licenses/>.
+**
+** ===========================================================================
+*/
 
 #ifndef ASK_SERVER_PROTOCOL_H
 #define ASK_SERVER_PROTOCOL_H
